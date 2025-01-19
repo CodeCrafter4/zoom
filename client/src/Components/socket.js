11b5f981b27b73b9621fdf85272d8776a44663e6
muhammedAbdellayassin <ssswar367@gmail.com>
2025-01-19 14:06:25 +0300

    backend url added

--- client/src/Components/socket.js
+++ client/src/Components/socket.js
@@ -1,6 +1,8 @@
 
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3000", { transports: ["polling", "websocket"] });
+const socket = io("https://zoom-lime-sigma.vercel.app", {
+  transports: ["polling", "websocket"],
+});
 
 export default socket;
